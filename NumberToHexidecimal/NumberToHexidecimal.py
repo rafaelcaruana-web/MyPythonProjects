5ac270b2c86022f8df947cb1964e1d8414bd59cb
rafael caruana <rafaelcaruana@outlook.com>
2026-06-19 20:43:38 +0100

    Add decimal to hexadecimal converter
    Implement a program to convert decimal numbers to hexadecimal and allow user to continue or quit.

---
+++ NumberToHexidecimal/NumberToHexidecimal.py
@@ -0,0 +1,18 @@
+playing = True
+
+while playing == True: 
+    number = int(input("Enter a number to convert to hexadecimal: "))
+    hexadecimal = hex(number)[2:].upper()
+
+    print("Your number in hexidecimal is", hexadecimal)
+
+    restart = input("Would you like to continue or quit? ('C' to continue, 'Q' to quit): ")
+    if restart == "C":
+        playing = True
+        number = 0
+    elif restart == "Q":
+        playing = False
+
+        print("You quit the program.")
+        number = 0
+        break
